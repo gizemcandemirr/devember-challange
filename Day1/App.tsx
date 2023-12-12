cd3581250c7af023615d9c9f095b177470e00392
Gizem Candemir <gizem.candemir@marketcalculus.ai>
2023-12-12 17:38:49 +0300

    Create Boxes With Flatlist

--- Day1/App.tsx
+++ Day1/App.tsx
@@ -1,11 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, FlatList } from "react-native";
 
+const days = [...Array(24)].map((val, index) => index + 1);
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <FlatList
+        data={days}
+        contentContainerStyle={styles.content}
+        columnWrapperStyle={styles.column}
+        numColumns={2}
+        renderItem={({ item }: any) => (
+          <View style={styles.box}>
+            <Text style={styles.text}>{item}</Text>
+          </View>
+        )}
+      />
+
+      <View>
+        <StatusBar style="auto" />
+      </View>
     </View>
   );
 }
@@ -13,8 +27,27 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+  },
+  content: {
+    gap: 10,
+    padding: 10,
+  },
+  column: {
+    gap: 10,
+  },
+  box: {
+    backgroundColor: "#f9ede3",
+    flex: 1,
+    aspectRatio: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    borderColor: "#9b4521",
+    borderWidth: StyleSheet.hairlineWidth,
+    borderRadius: 20,
+  },
+  text: {
+    color: "#9b4521",
+    fontSize: 70,
   },
 });
